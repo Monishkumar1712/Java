5331a20eda2c28b1eaad0c9d5316820a74af1810
Monishkumar1712 <123053853+Monishkumar1712@users.noreply.github.com>
2023-07-21 00:25:35 -1200

    Add files via upload

---
+++ divide.java
@@ -0,0 +1,12 @@
+package basicprogram;
+
+public class divide {
+
+	public static void main(String[] args) {
+		int a=50;
+		int b=5;
+		System.out.println(a/b);
+
+	}
+
+}
---
+++ sample.java
@@ -0,0 +1,20 @@
+package basicprogram;
+
+public class sample {
+
+	public static void main(String[] args) {
+		String[] lines={
+				" J a v v a",
+				" J aa v v aa",
+				"J J aaaaa V V aaaaa",
+				" JJ a  a V a  a"
+				
+				};
+		for (String line : lines) {
+            System.out.println(line);
+			
+		}
+
+	}
+
+}
